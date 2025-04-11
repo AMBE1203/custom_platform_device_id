7ce46a004651446918d360cca0be0f50bcbf4b83
Lệ Ngư Hà <duongthecuongbk227@gmail.com>
2025-04-11 18:39:06 +0700

    Delete lib directory

--- lib/custom_platform_device_id.dart
+++
@@ -1,6 +0,0 @@
-export 'package:platform_device_id/lib/platform_device_id.dart';
-export 'package:platform_device_id_macos/lib/platform_device_id_macos.dart';
-export 'package:platform_device_id_linux/lib/platform_device_id_linux.dart';
-export 'package:platform_device_id_windows/lib/platform_device_id_windows.dart';
-export 'package:platform_device_id_web/lib/platform_device_id_web.dart';
-export 'package:platform_device_id_platform_interface/lib/platform_device_id_platform_interface.dart'
