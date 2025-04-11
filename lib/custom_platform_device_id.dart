458c815ec1ce5079439579f89605337cfe45b9ec
Lệ Ngư Hà <duongthecuongbk227@gmail.com>
2025-04-11 18:25:31 +0700

    Update custom_platform_device_id.dart

--- lib/custom_platform_device_id.dart
+++ lib/custom_platform_device_id.dart
@@ -1,6 +1,6 @@
-export 'package:platform_device_id/platform_device_id.dart';
-export 'package:platform_device_id_macos/platform_device_id_macos.dart';
-export 'package:platform_device_id_linux/platform_device_id_linux.dart';
-export 'package:platform_device_id_windows/platform_device_id_windows.dart';
-export 'package:platform_device_id_web/platform_device_id_web.dart';
-export 'package:platform_device_id_platform_interface/platform_device_id_platform_interface.dart'
+export 'package:platform_device_id/lib/platform_device_id.dart';
+export 'package:platform_device_id_macos/lib/platform_device_id_macos.dart';
+export 'package:platform_device_id_linux/lib/platform_device_id_linux.dart';
+export 'package:platform_device_id_windows/lib/platform_device_id_windows.dart';
+export 'package:platform_device_id_web/lib/platform_device_id_web.dart';
+export 'package:platform_device_id_platform_interface/lib/platform_device_id_platform_interface.dart'
